254565027326bf1bc7722985ff16b60ee81748b5
namanchopra21087 <namanchopra21087@gmail.com>
2022-05-05 18:42:48 +0530

    Initial apache camel configuration

---
+++ src/test/java/com/naman/enterprise/integration/apcahe/camel/ApplicationTests.java
@@ -0,0 +1,11 @@
+package com.naman.enterprise.integration.apcahe.camel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+class ApplicationTests {
+
+	void contextLoads() {
+	}
+
+}
